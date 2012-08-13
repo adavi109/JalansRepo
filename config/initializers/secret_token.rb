62ede8231c6453c7f573a4b66f1b101ef73f6901
alan <alan@alan-VirtualBox.(none)>
2012-08-12 23:42:57 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolProj::Application.config.secret_token = 'f2078a4d9679f1b8a878f5e7bfd7f66dbfc5db42c1f8270a300a25da92d89a7db3235af90f848c935287c8774ed3ee164dadbe69ac356bc8d406dbd1a6ac01a2'
